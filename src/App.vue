5a5a91189d41c00d5437ed13c8f5b198c9667b5c
MateuszSutor <mateusz.sutor@icloud.com>
2020-05-10 18:18:13 +0200

    Adding and import new components.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
+
         <h1>Witaj w systemie zapisów na zajecia</h1>
+
         <div v-if="authenticatedUsername != ''">
             <h3 v-if="authenticatedUsername">Jestes zalogowany jako: {{authenticatedUsername}}</h3>
             <a @click="logMeOut">Wyjdz</a>
         </div>
+
         <div v-else>
             <login-form @login="logMeIn($event)"></login-form>
 
         </div>
-
 
     </div>
 </template>
@@ -17,9 +19,12 @@
 <script>
     import "milligram";
     import LoginForm from "./LoginForm";
+    import MeetingPage from "./meetings/MeetingPage";
+    import MeetingsList from "./meetings/MeetingsList";
+    import NewMeetingForm from "./meetings/NewMeetingForm";
 
     export default {
-        components: {LoginForm},
+        components: {LoginForm, MeetingPage},
         data() {
             return {
                 email: '',
--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -3,6 +3,7 @@
         <label>Zaloguj się e-mailem</label>
         <input type="email" v-model="email">
         <button @click="enter()">Wchodzę</button>
+
     </div>
 </template>
 
