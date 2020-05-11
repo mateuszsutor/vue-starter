00845fef9af9fd4f77b65036ed23f08cda4736f9
MateuszSutor <mateusz.sutor@icloud.com>
2020-05-11 18:47:20 +0200

    Final component MeetingsList

--- src/meetings/MeetingsList.vue
+++ src/meetings/MeetingsList.vue
@@ -1,34 +1,52 @@
 <template>
-    <table v-if="meetings.length > 0">
-        <thead>
-        <tr>
-            <th>Nazwa spotkania</th>
-            <th>Opis</th>
-            <th>Użytkownik</th>
-            <th></th>
-            
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="meeting in meetings" :key="meeting.name">
-            <td>{{ meeting.name }}</td>
-            <td>{{ meeting.description }}</td>
-            <td>{{meeting.meetingUsers}}</td>
-            <td>
-                <button class="button button-outline"
-                        @click="registerToMeeting(meeting)">Zapisz się</button>
+    <div>
+        <div v-if="meetings.length > 0">
+            <h3>Zaplanowane zajęcia ({{ meetings.length }})</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Nazwa spotkania</th>
+                    <th>Opis</th>
+                    <th>Użytkownik</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="meeting in meetings" :key="meeting.name">
+                    <td>{{ meeting.name }}</td>
+                    <td>{{ meeting.description }}</td>
+                    <td>
+                        <ul>
+                            <li v-for="user in meeting.meetingUsers" :key="user">
+                                {{user}}
+                            </li>
+                        </ul>
+                    </td>
+
+                    <td class="button-controller">
+                        <button class="button button-outline"
+                                @click="registerToMeeting(meeting)"
+                                v-if="meeting.meetingUsers.indexOf(user) < 0 ">Zapisz się</button>
+
+                        <button class="button button-outline" @click="leaveMeeting(meeting)"
+                                v-else>Wypisz się</button>
+
+                        <button class="button"
+                                id="deleteButton"
+                                @click="deleteEmptyMeeting(meeting)"
+                                v-if="meeting.meetingUsers.length === 0">Usuń puste spotkanie</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>
+            <p>Brak zaplanowanych spotkań</p>
+        </div>
+    </div>
 
 
-                <button class="button button-outline"
-                        @click="leaveMeeting(meeting)">Wypisz się</button>
 
-
-                <button class="button"
-                        @click="deleteEmptyMeeting(meeting)">Usuń puste spotkanie</button>
-            </td>
-        </tr>
-        </tbody>
-    </table>
 </template>
 
 <script>
@@ -36,17 +54,23 @@
         props: ['meetings', 'user'],
 
         methods: {
-            leaveMeeting(meeting){
+            leaveMeeting(meeting) {
                 this.$emit('leave', meeting)
             },
 
-            registerToMeeting(meeting){
+            registerToMeeting(meeting) {
                 this.$emit('register', meeting)
             },
 
-            deleteEmptyMeeting(meeting){
+            deleteEmptyMeeting(meeting) {
                 this.$emit('delete', meeting)
             }
         }
     }
 </script>
+
+<style>
+    .button-controller {
+        text-align: right;
+    }
+</style>
